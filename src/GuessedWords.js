13d9fedfd0af8e80f30fdcd0982e3555e87ce59e
He Wang <r06631013@ntu.edu.tw>
2020-01-09 23:30:39 +0800

    Tests and code for non-zero guessed words

--- src/GuessedWords.js
+++ src/GuessedWords.js
@@ -5,6 +5,27 @@ const GuessedWords = props => {
   let contents;
   if (props.guessedWords.length == 0) {
     contents = <span data-test='guess-instructions'>Try to guess the secret word!</span>;
+  } else {
+    const guessedWordsRows = props.guessedWords.map((word, index) => (
+      <tr key={index} data-test='guessed-word'>
+        <td>{word.guessedWord}</td>
+        <td>{word.letterMatchCount}</td>
+      </tr>
+    ));
+    contents = (
+      <duv data-test='guessed-words'>
+        <h3>Guessed Words</h3>
+        <table>
+          <thead>
+            <tr>
+              <th>Guess</th>
+              <th>Matching Letters</th>
+            </tr>
+          </thead>
+          <tbody>{guessedWordsRows}</tbody>
+        </table>
+      </duv>
+    );
   }
   return <div data-test='component-guessed-words'>{contents}</div>;
 };
