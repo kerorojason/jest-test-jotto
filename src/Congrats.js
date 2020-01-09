2e302fd56650119a183a2fd92955fd8a76e9f030
He Wang <r06631013@ntu.edu.tw>
2020-01-09 15:06:18 +0800

    add propTypes for Congrats component

--- src/Congrats.js
+++ src/Congrats.js
@@ -2,7 +2,8 @@ import React from 'react';
 /**
  * Functional react component for congratulatory message
  * @function
- * @returns {JSX.Element}
+ * @param {object} props - React props.
+ * @returns {JSX.Element} - Rendered component (or null if 'success props is false')
  */
 export default props => {
   if (props.success) {
