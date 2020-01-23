a3db115d6de0a1db2fc86166dfd5865feffcdc2b
He Wang <r06631013@ntu.edu.tw>
2020-01-23 22:48:42 +0800

    Set up Input component and storeFactory test util

--- test/testUtils.js
+++ test/testUtils.js
@@ -1,12 +1,12 @@
 import checkPropTypes from 'check-prop-types';
+import { createStore } from 'redux';
 
-/**
- * Return ShallowWrapper containing node(s) with the fiven data-test value.
- * @function findByTestAttr
- * @param {ShallowWrapper} wrapper - Enzyme shallow wrapper to search within.
- * @param {string} val - Value of data-test attribute for search
- * @returns {ShallowWrapper}
- */
+import rootReducer from '../src/reducers';
+
+export const storeFactory = initialState => {
+  return createStore(rootReducer, initialState);
+};
+
 export const findByTestAttr = (wrapper, val) => {
   return wrapper.find(`[data-test='${val}']`);
 };
