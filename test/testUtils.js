d78e10101a77f3c94b07e892cb7820df327fc256
He Wang <r06631013@ntu.edu.tw>
2020-01-24 11:43:32 +0800

    Install Redux Thunk middleware

--- test/testUtils.js
+++ test/testUtils.js
@@ -1,7 +1,8 @@
 import checkPropTypes from 'check-prop-types';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 
 import rootReducer from '../src/reducers';
+import { middlewares } from '../src/configureStore';
 
 export const storeFactory = initialState => {
   return createStore(rootReducer, initialState);
